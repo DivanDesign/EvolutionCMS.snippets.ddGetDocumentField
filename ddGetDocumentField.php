09cd851cd83cc41d94e2a979f81d4e7d0cbfa175
Ilyas Ronef <ilyas@divandesign.biz>
2016-12-27 19:01:23 +0500

    * Minor changes: Comments and code style.

--- ddGetDocumentField.php
+++ ddGetDocumentField.php
@@ -1,25 +1,26 @@
 <?php
 /** 
- * ddGetDocumentField.php
+ * ddGetDocumentField
  * @version 2.5 (2014-06-05)
  * 
  * @desc Snippet gets the necessary document fields (and TV) by its id.
  * 
- * @uses The library modx.ddTools 0.12.
- * @uses The snippet ddTypograph 2.2 (if typographing is required).
+ * @uses MODXEvo.library.ddTools >= 0.12.
+ * @uses MODXEvo.snippet.ddTypograph >= 2.2 (if typographing is required).
  * 
- * @param $id {integer} - Document identifier. Default: current document.
- * @param $field {comma separated string; separated string} - Documents fields to get separated by commas. Fields and their aliases must be separated by “::” if aliases are required while returning the results (for example: 'pagetitle::title,content::text'). See the examples below. @required
- * @param $alternateField {comma separated string} - Alternate fields to get if the main is empty. Default: ''.
- * @param $typography {0; 1} - Need to typography result? Default: 0.
- * @param $tpl {string: chunkName} - Chunk to parse result. Default: ''.
- * @param $glue {string} - String for join the fields. Default: ''.
- * @param $outputFormat {''; 'json'} - Output format. Default: ''.
- * @param $placeholders {separated string} - Additional data to be transfered. Format: string, separated by '::' between a pair of key-value, and '||' between the pairs. Default: ''.
- * @param $mode {''; 'ajax'} - Режим работы. If mode is AJAX, the id gets from the $_REQUEST array. Use the “securityFields” param! Default: ''.
- * @param $securityFields {separated string} - The fields for security verification. Format: field:value|field:value|etc. Default: ''. 
- * @param $escaping {0; 1} - Need to escape special characters from result? Default: 0.
- * @param $urlencode {0; 1} - Need to URL-encode result string? Default: 0.
+ * @param $id {integer} — Document identifier. Default: current document.
+ * @param $field {string_commaSeparated} — Documents fields to get separated by commas. @required
+ * @param $field[] {string|string_separated} — Fields and their aliases must be separated by “::” if aliases are required while returning the results (for example: 'pagetitle::title,content::text'). @required
+ * @param $alternateField {string_commaSeparated} — Alternate fields to get if the main is empty. Default: ''.
+ * @param $tpl {string_chunkName} — Chunk to parse result. Default: ''.
+ * @param $glue {string} — String for join the fields. Default: ''.
+ * @param $typography {0|1} — Need to typography result? Default: 0.
+ * @param $outputFormat {''|'json'} — Output format. Default: ''.
+ * @param $placeholders {string_separated} — Additional data to be transfered. Format: string, separated by '::' between a pair of key-value, and '||' between the pairs. Default: ''.
+ * @param $mode {''|'ajax'} — Режим работы. If mode is AJAX, the id gets from the $_REQUEST array. Use the “securityFields” param! Default: ''.
+ * @param $securityFields {string_separated} — The fields for security verification. Format: field:value|field:value|etc. Default: ''. 
+ * @param $escaping {0|1} — Need to escape special characters from result? Default: 0.
+ * @param $urlencode {0|1} — Need to URL-encode result string? Default: 0.
  * 
  * @link http://code.divandesign.biz/modx/ddgetdocumentfield/2.5
  * 
@@ -45,7 +46,10 @@ if (isset($field)){
 	$outputFormat = isset($outputFormat) ? strtolower($outputFormat) : '';
 	
 	//Если данные нужно получать аяксом
-	if (isset($mode) && strtolower($mode) == 'ajax'){
+	if (
+		isset($mode) &&
+		strtolower($mode) == 'ajax'
+	){
 		$id = $_REQUEST['id'];
 		
 		//Если заданы поля для проверки безопасности
@@ -65,7 +69,12 @@ if (isset($field)){
 			$docSecurityFields = ddTools::getTemplateVarOutput($securityFields, $id);
 			
 			//Если по каким-то причинам ничего не получили — прерываем
-			if (!$docSecurityFields || count($docSecurityFields) == 0){return;}
+			if (
+				!$docSecurityFields ||
+				count($docSecurityFields) == 0
+			){
+				return;
+			}
 			
 			//Перебираем полученные значения, если хоть одно не совпадает с условием — прерываем
 			foreach ($docSecurityFields as $key => $val){
@@ -90,8 +99,12 @@ if (isset($field)){
 		//Просто разбиваем по запятой
 		$field = explode(',', $field);
 		
+		//For backward compatibility
 		//Если задан устаревший параметр «$numericNames»
-		if (isset($numericNames) && $numericNames == '1'){
+		if (
+			isset($numericNames) &&
+			$numericNames == '1'
+		){
 			//Ругаемся
 			$modx->logEvent(1, 2, '<p>The “numericNames” parameter is deprecated. You can pass aliases inside of the “field” parameter instead.</p><p>The snippet has been called in the document with id '.$modx->documentIdentifier.'.</p>', $modx->currentSnippet);
 			
@@ -141,7 +154,10 @@ if (isset($field)){
 		//Если надо было вернуть ещё и url документа и Если такого поля нет или оно пусто (а то мало ли, как TV назвали)
 		if (
 			array_search('url', $field) !== false &&
-			(!isset($result['url']) || trim($result['url']) == '')
+			(
+				!isset($result['url']) ||
+				trim($result['url']) == ''
+			)
 		){
 			$result['url'] = $modx->makeUrl($id);
 		}
